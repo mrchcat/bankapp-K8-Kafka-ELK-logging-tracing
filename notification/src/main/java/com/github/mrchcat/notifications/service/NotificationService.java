4389f4344d6708d78a9bfd98f6184f6b8ad16119
mrchcat <m-cat@mail.ru>
2025-09-16 22:54:19 +0300

    feat: bank-notifications read from kafka

--- notification/src/main/java/com/github/mrchcat/notifications/service/NotificationService.java
+++ notification/src/main/java/com/github/mrchcat/notifications/service/NotificationService.java
@@ -1,10 +1,50 @@
 package com.github.mrchcat.notifications.service;
 
+import com.github.mrchcat.notifications.Repository.NotificationRepository;
+import com.github.mrchcat.notifications.domain.BankNotification;
 import com.github.mrchcat.shared.notification.BankNotificationDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
-public interface NotificationService {
+import java.time.LocalDateTime;
 
-    void save(BankNotificationDto dto);
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationService {
 
+    private final NotificationRepository notificationRepository;
+
+    @KafkaListener(topics = {"#{'${application.kafka.topic.notifications}'.split(',')}"})
+    public void readNotifications(BankNotificationDto dto) {
+        var notification = BankNotification.builder()
+                .service(dto.service())
+                .username(dto.username())
+                .fullName(dto.fullName())
+                .email(dto.email())
+                .message(dto.message())
+                .isProcessed(false)
+                .createdAt(LocalDateTime.now())
+                .build();
+        notificationRepository.save(notification);
+    }
+
+    @Scheduled(fixedDelay = 1000L)
+    public void process() {
+        notificationRepository.findAllNotProcessed()
+                .forEach(notification -> {
+                    sendByEmail(notification);
+                    notificationRepository.setProcessed(notification.getId());
+                });
+    }
+
+    private void sendByEmail(BankNotification notification) {
+        String message = "Уведомление " + notification.toString() + " отправлено по почте";
+        log.info(message);
+    }
 
 }
