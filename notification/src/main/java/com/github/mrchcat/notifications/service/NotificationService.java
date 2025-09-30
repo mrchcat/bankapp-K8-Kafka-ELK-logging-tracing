381a13f4a36fcc372d162ff758b11cbfda1040a0
mrchcat <m-cat@mail.ru>
2025-10-01 00:50:29 +0300

    feat: add delivery to e-mail in notification service

--- notification/src/main/java/com/github/mrchcat/notifications/service/NotificationService.java
+++ notification/src/main/java/com/github/mrchcat/notifications/service/NotificationService.java
@@ -5,7 +5,12 @@ import com.github.mrchcat.notifications.domain.BankNotification;
 import com.github.mrchcat.shared.notification.BankNotificationDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +23,15 @@ import java.time.LocalDateTime;
 public class NotificationService {
 
     private final NotificationRepository notificationRepository;
+    private final MailSender mailSender;
+
+    @Value("${application.mail.from_address}")
+    private String fromMailAddress;
+    @Value("${application.mail.sender}")
+    private String senderName;
+    @Value("${application.mail.enabled}")
+    private boolean isEmailEnabled;
+
 
     @KafkaListener(topics = {"#{'${application.kafka.topic.notifications}'.split(',')}"})
     @Transactional("transactionManager")
@@ -38,14 +52,24 @@ public class NotificationService {
     public void process() {
         notificationRepository.findAllNotProcessed()
                 .forEach(notification -> {
-                    sendByEmail(notification);
-                    notificationRepository.setProcessed(notification.getId());
+                    if (!isEmailEnabled || sendByEmail(notification)) {
+                        notificationRepository.setProcessed(notification.getId());
+                    }
                 });
     }
 
-    private void sendByEmail(BankNotification notification) {
-        String message = String.format("Уведомление \"%s\" отправлено по почте", notification);
-        log.info(message);
+    private boolean sendByEmail(BankNotification notification) {
+        try {
+            SimpleMailMessage msg = new SimpleMailMessage();
+            msg.setFrom(fromMailAddress);
+            msg.setTo(notification.getEmail());
+            msg.setText(notification.getMessage());
+            mailSender.send(msg);
+            log.info("Уведомление \"{}\" отправлено на почту {}", notification.getMessage(), notification.getEmail());
+            return true;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return false;
+        }
     }
-
 }
